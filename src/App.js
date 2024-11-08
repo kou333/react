6d40ad82da40bcd73f9c9e03870715195a004f1d
kou333 <naikkoustubh@gmail.com>
2024-11-08 21:02:01 +0530

    Initial commit for specific folder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Accordian from './components/accordian';
+
+function App() {
+  return (
+    <div className="App">
+   {/*Accordian   */}
+        
+   <Accordian/>
+    </div>
+  );
+}
+
+export default App;
